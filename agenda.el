fc685a4f43f4f6e13436bedf2d1eb7a0f9d00294
trémeur <eheu48@gmail.com>
2020-05-20 18:18:14 +0100

    Add files

---
+++ agenda.el
@@ -0,0 +1,13 @@
+(setq org-agenda-files '("~/Documents/drive/org/calendar"))
+(setq org-agenda-prefix-format '((agenda . "%?-12t%-30b%s")))
+(setq org-agenda-show-current-time-in-grid nil)
+(setq org-deadline-warning-days 0)
+(setq org-agenda-skip-scheduled-if-done t)
+(setq org-agenda-skip-deadline-if-done t)
+(setq calendar-day-name-array ["de Sul" "de Lun" "de Meurth" "de Mergher"
+			       "de Yow" "de Gwener" "de Sadorn"])
+(setq calendar-day-abbrev-array ["Su" "L" "Mth" "Mr" "Y" "G" "Sa"])
+(setq calendar-month-name-array ["mis Genver" "mis Whevrel" "mis Meur’"
+				 "mis Ebrel" "mis Me" "mis Efen"
+				 "mis Gorefen" "mis Est" "mis Gwyngala"
+				 "mis Hedra" "mis Du" "mis Kevardhu"])
