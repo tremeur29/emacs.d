8b808390c53e7f99e97b70b0c463b9388db0bf8e
trémeur <eheu48@gmail.com>
2020-08-03 12:24:02 +0100

    Add org-drill and an org-capture template for it

--- init.el
+++ init.el
@@ -258,6 +258,113 @@
   (org-ellipsis " ⤵")
   :hook (org-mode . org-bullets-mode))
 
+(setq org-capture-templates
+	     '(("c" "LCLB" entry
+	       (file "~/Documents/drive/org/lclb.org")
+	       "* %^t
+** 1                                                                 :drill:
+:PROPERTIES:
+:DRILL_CARD_TYPE: twosided
+:END:
+*** Cornish
+%^{Word 1 (Cornish)}
+*** English
+%^{Word 1 (English)}
+*** Type
+%^{Word 1 type||masculine|feminine|plural|preterite}
+** 2                                                                 :drill:
+:PROPERTIES:
+:DRILL_CARD_TYPE: twosided
+:END:
+*** Cornish
+%^{Word 2 (Cornish)}
+*** English
+%^{Word 2 (English)}
+*** Type
+%^{Word 2 type||masculine|feminine|plural|preterite}
+** 3                                                                 :drill:
+:PROPERTIES:
+:DRILL_CARD_TYPE: twosided
+:END:
+*** Cornish
+%^{Word 3 (Cornish)}
+*** English
+%^{Word 3 (English)}
+*** Type
+%^{Word 3 type||masculine|feminine|plural|preterite}
+** 4                                                                 :drill:
+:PROPERTIES:
+:DRILL_CARD_TYPE: twosided
+:END:
+*** Cornish
+%^{Word 4 (Cornish)}
+*** English
+%^{Word 4 (English)}
+*** Type
+%^{Word 4 type||masculine|feminine|plural|preterite}
+** 5                                                                 :drill:
+:PROPERTIES:
+:DRILL_CARD_TYPE: twosided
+:END:
+*** Cornish
+%^{Word 5 (Cornish)}
+*** English
+%^{Word 5 (English)}
+*** Type
+%^{Word 5 type||masculine|feminine|plural|preterite}
+** 6                                                                 :drill:
+:PROPERTIES:
+:DRILL_CARD_TYPE: twosided
+:END:
+*** Cornish
+%^{Word 6 (Cornish)}
+*** English
+%^{Word 6 (English)}
+*** Type
+%^{Word 6 type||masculine|feminine|plural|preterite}
+** 7                                                                 :drill:
+:PROPERTIES:
+:DRILL_CARD_TYPE: twosided
+:END:
+*** Cornish
+%^{Word 7 (Cornish)}
+*** English
+%^{Word 7 (English)}
+*** Type
+%^{Word 7 type||masculine|feminine|plural|preterite}
+** 8                                                                 :drill:
+:PROPERTIES:
+:DRILL_CARD_TYPE: twosided
+:END:
+*** Cornish
+%^{Word 8 (Cornish)}
+*** English
+%^{Word 8 (English)}
+*** Type
+%^{Word 8 type||masculine|feminine|plural|preterite}
+** 9                                                                 :drill:
+:PROPERTIES:
+:DRILL_CARD_TYPE: twosided
+:END:
+*** Cornish
+%^{Word 9 (Cornish)}
+*** English
+%^{Word 9 (English)}
+*** Type
+%^{Word 9 type||masculine|feminine|plural|preterite}
+** 10                                                                :drill:
+:PROPERTIES:
+:DRILL_CARD_TYPE: twosided
+:END:
+*** Cornish
+%^{Word 10 (Cornish)}
+*** English
+%^{Word 10 (English)}
+*** Type
+%^{Word 10 type||masculine|feminine|plural|preterite}")))
+
+(use-package org-drill)
+
 (global-set-key (kbd "C-c a") 'org-agenda)
 
 (setq calendar-week-start-day 1)
